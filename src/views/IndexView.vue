ffcbafe3b2598cdf917a400cba387b40e3ff3c35
PuntoCiegoF <fernando.azuara@outlook.es>
2025-01-06 07:08:00 -0600

    Update IndexView.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -181,15 +181,6 @@ onMounted(() => {
   width: 100%;
   height: 500px; /* O el tamaño deseado del video */
   background-color: #000; /* Simulando el fondo del video */
-}
-  .responsive-image {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  }
-  
-  @media (max-width: 600px) {
-  .responsive-image {
   }
 }
 </style>
