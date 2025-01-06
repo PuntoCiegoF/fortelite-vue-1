a48525ca3ad7cbf8d767ed653ac415ce630dcdb9
PuntoCiegoF <fernando.azuara@outlook.es>
2025-01-06 06:05:06 -0600

    Update IndexView.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -8,7 +8,11 @@
         type="video/mp4">
       Tu navegador no soporta el elemento de video.
     </video>
-     <img loading="lazy" class="w-full aspect-square image-custom-size" src="https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/08/yyyy44.png" style="width: 1300px; height: 152px; object-fit: cover; margin-top: 20px; margin-bottom: 20px; margin-left: 20px; margin-right: 20px;" />
+    <img loading="lazy" 
+     class="w-full aspect-square"
+     src="https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/08/yyyy444.png"
+     style="width: 1300px; height: 152px; object-fit: cover; margin-top: 20px; margin-bottom: 20px; margin-left: 20px; margin-right: 20px;" 
+     onload="if(window.innerWidth <= 768) { this.style.width = '100%'; this.style.height = 'auto'; this.style.marginLeft = '10px'; this.style.marginRight = '10px'; this.style.marginTop = '10px'; this.style.marginBottom = '10px'; }" />
     </div>
     <div class="relative">
       <ContainerComponent tag="article">
