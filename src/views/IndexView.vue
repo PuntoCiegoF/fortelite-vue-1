463045df622799ff9091bb87f211733b92bec47c
PuntoCiegoF <fernando.azuara@outlook.es>
2025-06-03 00:15:10 -0600

    Update IndexView.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -14,6 +14,14 @@
      srcset="https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/08/cert03062025.png 1300w, 
             https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/08/cert03062025.png 715w"  
      sizes="(max-width: 768px) 715px, 1300px" />
+    <a href="https://iq.ul.com/ul/cert.aspx?ULID=104745228" 
+         target="_blank" 
+         class="btn-certificacion"
+         style="position: absolute; top: 35px; left: 110px; background-color: rgba(0,123,255,0.9); color: white; padding: 5px 10px; border-radius: 4px; font-size: 12px; text-decoration: none;"
+      </a>
+    </div>
+  </div>
+</template>
     </div>
     <div class="relative">
       <ContainerComponent tag="article">
