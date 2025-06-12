8246219c9f567b5b52469c1f6c64b7d6ad48caeb
PuntoCiegoF <fernando.azuara@outlook.es>
2025-06-12 10:35:35 -0600

    Update contact.js

--- src/constants/contact.js
+++ src/constants/contact.js
@@ -1,5 +1,5 @@
 export const CONTACT = {
-    title: "Contacto",
+    title: "CONTACTO",
     buttom_image: "https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/Captura-de-pantalla-2024-07-21-a-las-12.46.58%E2%80%AFp.m-e1721587699795.png",
     top_banner: "https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/Captura-de-pantalla-2024-07-21-a-las-3.01.31%E2%80%AFp.m.png",
     form_endpoint : process.env.VUE_APP_API_URL + "/form-entry",
