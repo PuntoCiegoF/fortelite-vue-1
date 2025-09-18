ad5111aa291a7e77d20a7aff7bf84a110ca95cb5
PuntoCiegoF <fernando.azuara@outlook.es>
2025-09-17 19:35:58 -0600

    Create FinishesView.vue

---
+++ src/views/FinishesView.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Reutilizamos tu plantilla de segmentos -->
+  <SegmentTemplate :segment="segment" />
+</template>
+
+<script setup>
+import SegmentTemplate from '@/components/segments/SegmentTemplate.vue'
+import FINISHES_ONLY from '@/constants/segments/finishes-only'
+
+const segment = FINISHES_ONLY
+</script>
