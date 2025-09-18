5a5bd3aecf468b50f7bdb1a22333044c905d186e
PuntoCiegoF <fernando.azuara@outlook.es>
2025-09-17 19:14:04 -0600

    Create finishes-only.js

---
+++ src/constants/segments/finishes-only.js
@@ -0,0 +1,51 @@
+const FINISHES_ONLY = {
+  bannerTop: {
+    image:
+      'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/iStock-1495400812-scaled.jpg',
+    text: 'Dale el toque final perfecto',
+  },
+  title: 'ACABADOS',
+  // No definimos segmentImages ni infoList → solo se mostrará la sección "Acabados"
+  finishes: {
+    showBar: true,
+    list: [
+      {
+        text: 'Laminado mate',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/laminado-mate-2.jpg',
+      },
+      {
+        text: 'Laminado brillante',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/laminado-brillante-2.jpg',
+      },
+      {
+        text: 'Hot foil',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/hot-foil-2.jpg',
+      },
+      {
+        text: 'Cold foil',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/cold-foil-6.jpg',
+      },
+      {
+        text: 'Barniz UV',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/barniz-uv-6.jpg',
+      },
+      {
+        text: 'Emboss',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/emboss-6.jpg',
+      },
+      {
+        text: 'Deboss',
+        image:
+          'https://lawngreen-wallaby-976278.hostingersite.com/wp-content/uploads/2024/07/deboss-2-6.webp',
+      },
+    ],
+  },
+};
+
+export default FINISHES_ONLY;
